aae78438eab72bf55411f48dff49352dc0ff19c4
Thamiris Balbi <thamiris.balbi@estudante.ufjf.br>
2022-03-11 20:55:56 -0300

    Adiciona comentários em index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom'; //dom é a árvore de elementos
-import App from './App';
+import App from './App'; //importa de App.js
 
 ReactDOM.render(
     <App />,
   document.getElementById('root')
-);
+); //coloca em tela o armazenado pela variável App dentro do Id root pelo comando document.getElementById('root') 
+
 
 
