64f4c4dcdf70f4f04735e7d0a46a03f4eb1029ed
Thamiris Balbi <thamiris.balbi@estudante.ufjf.br>
2022-03-11 21:40:08 -0300

    Adiciona propriedade de título em Header

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
-export default function Header() { //export default exporta o componente 
+export default function Header(props) { //export default exporta o componente 
     return (
         <header>
             <h1>
-                Be The Hero
+                {props.title}
             </h1>
         </header>
 
     );
 }
-
+//Aqui, a prop tem função de buscar o título definido em App.js para também ser executado no programa
+//Para isso, é passado props como parâmetro
