897db02dc0f6d81518375b9cfc709ef050882a94
Thamiris Balbi <thamiris.balbi@estudante.ufjf.br>
2022-03-11 21:22:40 -0300

    Importa Header para App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import Header from './Header';
+
 function App() {
   return (
-    <h1> Hello World! </h1>
+    <Header /> //Header não tem conteúdo, então fecha-se a tag nela mesma
   );
 }
 
