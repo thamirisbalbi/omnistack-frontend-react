60d51a020c44cbd7f29ef1b276217f28a8b93323
Thamiris Balbi <thamiris.balbi@estudante.ufjf.br>
2022-03-14 13:17:51 -0300

    Adiciona outra imagem à aplicação

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import './styles.css';
 
+import logoImg from '../../assets/logo.png'
 import heroesImg from '../../assets/heroes.png';
 
 
@@ -9,8 +10,9 @@ export default function Login() {
     return (
         <div className= "login-container">
             <section className="form">
-
+                <img src={logoImg} alt="Be The Hero" />
             </section>
+
             <img src={heroesImg} alt="Heroes" /> 
         </div>
 
