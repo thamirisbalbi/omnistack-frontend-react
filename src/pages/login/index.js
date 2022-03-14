68a54ad34f93eeaeb8e8fa9efe5081e9dfbf579b
Thamiris Balbi <thamiris.balbi@estudante.ufjf.br>
2022-03-14 13:00:11 -0300

    Prepara subdivisões para estilização da página

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 import './styles.css';
 
+
 export default function Login() {
     return (
-        <h1>Hello World</h1>
+        <div className= "login-container">
+            <section className="form">
+
+            </section>
+        </div>
+
     );
 }
