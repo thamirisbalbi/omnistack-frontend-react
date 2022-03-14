618e79a84c8e82ccf7ebad91cae7fc5abb1b61f4
Thamiris Balbi <thamiris.balbi@estudante.ufjf.br>
2022-03-14 12:41:08 -0300

    Importa o arquivo global para a aplicação app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './global.css';
+
 import Login from './pages/Login'; //quando selecionamos uma pasta, automaticamente o sistema procura o arquivo index de dentro
 
 function App() {
