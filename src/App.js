1ccef4c299222aca1ee821d7b1b9ea985501b6df
Thamiris Balbi <thamiris.balbi@estudante.ufjf.br>
2022-03-14 12:11:45 -0300

    Deleta código do App.js

--- src/App.js
+++ src/App.js
@@ -3,16 +3,6 @@ import React, { useState } from 'react';
 import Login from './pages/Login'; //quando selecionamos uma pasta, automaticamente o sistema procura o arquivo index de dentro
 
 function App() {
-  const [counter, setCounter] = useState(0); //setCounter é uma função que altera o valor de counter
-
-  //retorna um array[valor, funçãoDeAtualização]
-
-  function increment(){
-    setCounter(counter + 1);
-
-    console.log(counter);
-  }
-
   return (
     <div>
       <Header>Contador: {counter}</Header>
