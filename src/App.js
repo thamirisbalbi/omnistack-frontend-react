9bd21bded4ae46c031296e2594e488564de5be97
Thamiris Balbi <thamiris.balbi@estudante.ufjf.br>
2022-03-14 04:55:11 -0300

    Cria contador sem o conceito de estado

--- src/App.js
+++ src/App.js
@@ -3,10 +3,19 @@ import React from 'react';
 import Header from './Header';
 
 function App() {
+  let counter = 0;
+
+  function increment(){
+    counter += 1;
+
+    console.log(counter);
+  }
+
   return (
-    <Header>
-      Semana Omnistack
-    </Header>//Passa propriedade com o texto que gostaria que fosse exibido dentro do h1
+    <div>
+      <Header>Contador: {counter}</Header>
+      <button onClick={increment }>Incrementar</button>
+    </div>
   );
 }
 
