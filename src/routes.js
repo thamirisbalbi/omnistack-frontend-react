13eb576b6c2971a5a3bc63c476a9d65baec40eb6
Thamiris Balbi <thamiris.balbi@estudante.ufjf.br>
2022-03-17 22:28:37 -0300

    Cria função para as rotas

--- src/routes.js
+++ src/routes.js
@@ -3,3 +3,13 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Login from './pages/Login';
 
+export default function Routes() { //as rotas viram componentes
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path='/' component={Login} />
+            </Switch>
+
+        </BrowserRouter>
+    );
+}
