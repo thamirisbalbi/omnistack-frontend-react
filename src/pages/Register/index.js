a408583e3b115d8dbff2410fe4f210cc45983c81
Thamiris Balbi <thamiris.balbi@estudante.ufjf.br>
2022-03-17 23:53:17 -0300

    Detalha estrutura da página de registro

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 
 import './styles.css';
 
+import heroesImg from '../../assets/heroes.png';
+
 export default function Register() {
     return (
         <div className="register-container">
             <div className="content">
                 <section>
-                    
+                    <img src={heroesImg} alt="Be The Hero"/> 
+
+                    <h1>Cadastro</h1>
+                    <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
+
                 </section>
 
                 <form>
