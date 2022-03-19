4d079b18944017ee17fda15f958b06fb24ee6dcb
Thamiris Balbi <thamiris.balbi@estudante.ufjf.br>
2022-03-19 16:53:24 -0300

    Passa a mesma aplicação de backlink para Register

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -14,6 +14,10 @@ export default function Register() {
                     <h1>Cadastro</h1>
                     <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
 
+                    <Link className="back-link" to="/register">
+                        <FiLogIn size={16} color="#E02041" />
+                        Não tenho cadastro
+                    </Link> 
                 </section>
 
                 <form>
