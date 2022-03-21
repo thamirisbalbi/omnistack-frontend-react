a56069a16e23d81b201519e1c314aee5fcbaf28f
Thamiris Balbi <thamiris.balbi@estudante.ufjf.br>
2022-03-21 02:29:37 -0300

    Utiliza o código html de register para NewIncident

--- src/pages/NewIncident/index.js
+++ src/pages/NewIncident/index.js
@@ -3,5 +3,34 @@ import React from 'react';
 import './styles.css';
 
 export default function NewIncident() {
-    return <h1>New Incident</h1>
+    return (
+        <div className="register-container">
+            <div className="content">
+                <section>
+                    <img src={heroesImg} alt="Be The Hero" width="130" height="130"/> 
+
+                    <h1>Cadastro</h1>
+                    <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
+
+                    <Link className="back-link" to="/">
+                        <FiArrowLeft size={16} color="#E02041" />
+                        Não tenho cadastro
+                    </Link> 
+                </section>
+
+                <form>
+                    <input placeholder="Nome da ONG" />
+                    <input type="email" placeholder="E-mail" />
+                    <input placeholder= "WhatsApp" /> 
+
+                    <div className="input-group">
+                        <input placeholder="Cidade" />
+                        <input placeholder="UF" style= {{ width: 80 }} />  
+                    </div>
+
+                    <button className="button" type="submit" >Cadastrar</button>
+                </form>
+            </div>
+        </div>
+    );
 }
