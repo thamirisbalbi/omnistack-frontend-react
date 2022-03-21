522ab5bd2a9cdbf38e74071c9acf5d83e6a1d705
Thamiris Balbi <thamiris.balbi@estudante.ufjf.br>
2022-03-21 14:02:34 -0300

    Armazena cada valor dos inputs dentro de um estado

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi'; //importa ícone do feather icons
 
@@ -8,8 +8,23 @@ import './styles.css';
 import heroesImg from '../../assets/heroes.png';
 
 export default function Register() {
+    const [name, setName] = useState(''); //sintaxe: valor, função para atualizar o valor
+    const [email, setEmail] = useState('');
+    const [whatsapp, setWhatsapp] = useState('');
+    const [city, setCity] = useState('');
+    const [uf, setUf] = useState('');
+
     function handleRegister(e) { //'e' seria o evento do formulário
         e.preventDefault(); //previne o comportamento padrão do formulário 
+        
+        console.log({
+            name,
+            email,
+            whatsapp,
+            city,
+            uf,
+        })
+    
     }
 
     return (
@@ -28,13 +43,35 @@ export default function Register() {
                 </section>
 
                 <form onSubmit={handleRegister}>
-                    <input placeholder="Nome da ONG" />
-                    <input type="email" placeholder="E-mail" />
-                    <input placeholder= "WhatsApp" /> 
+                    <input 
+                        placeholder="Nome da ONG"
+                        value={name}
+                        onChange={e => setName(e.target.value)} //arrow function 
+                    />
+                    <input 
+                        type="email" 
+                        placeholder="E-mail"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
+                    <input 
+                        placeholder= "WhatsApp"
+                        value={whatsapp}
+                        onChange={e => setWhatsapp(e.target.value)}
+                    /> 
 
                     <div className="input-group">
-                        <input placeholder="Cidade" />
-                        <input placeholder="UF" style= {{ width: 80 }} />  
+                        <input 
+                            placeholder="Cidade" 
+                            value={city}
+                            onChange={e => setCity(e.target.value)}
+                        />
+                        <input 
+                            placeholder="UF" 
+                            style= {{ width: 80 }}
+                            value={uf}
+                            onChange={e => setUf(e.target.value)}
+                        />  
                     </div>
 
                     <button className="button" type="submit" >Cadastrar</button>
