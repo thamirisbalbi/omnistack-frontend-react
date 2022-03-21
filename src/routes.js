f9babc484bf28f54fe90625ae08b87fa7aa8f081
Thamiris Balbi <thamiris.balbi@estudante.ufjf.br>
2022-03-21 02:19:06 -0300

    Importa NewIncident para routes, e cria nova rota

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
+import NewIncident from './pages/NewIncident';
 
 export default function Routes() { //as rotas viram componentes
     return (
@@ -13,6 +14,7 @@ export default function Routes() { //as rotas viram componentes
                 <Route path="/register" component={Register} />
 
                 <Route path="/profile" component={Profile} /> 
+                <Route path="/incidents/new" component ={NewIncident} />
             </Switch>
         </BrowserRouter>
     );
