3a877ffe8a7890d6af5d2bbb57e79063307bf2ba
Thamiris Balbi <thamiris.balbi@estudante.ufjf.br>
2022-03-21 01:11:01 -0300

    Cria a estrutura do Profile no frontEnd

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,5 +1,21 @@
 import React from 'react'; 
+import { Link } from 'react-router-dom';
+import { FiPower } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.png';
 
 export default function Profile() {
-    return <h1>Profile</h1> 
+    return (
+        <div className="profile-container">
+            <header>
+                <img src={logoImg} alt="Be The Hero" />
+                <span>Bem vinda, APAD</span>
+
+                <Link className="button" to="/incidents/new">Cadastrar novo caso</Link> 
+                <button type="button">
+                    <FiPower size={18} color="#E02041" />
+                </button>
+            </header>
+        </div>
+    ); 
 }
