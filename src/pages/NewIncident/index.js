5b5d9b3cf1d56c5e5d05f51f83d48b1d21ea5c84
Thamiris Balbi <thamiris.balbi@estudante.ufjf.br>
2022-03-21 12:38:37 -0300

    Estruturação da página NewIncident

--- src/pages/NewIncident/index.js
+++ src/pages/NewIncident/index.js
@@ -1,32 +1,31 @@
 import React from 'react';
+import { Link } from  'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
 
 import './styles.css';
 
+import heroesImg from '../../assets/heroes.png';
+
 export default function NewIncident() {
     return (
-        <div className="register-container">
+        <div className="new-incident-container">
             <div className="content">
                 <section>
                     <img src={heroesImg} alt="Be The Hero" width="130" height="130"/> 
 
-                    <h1>Cadastro</h1>
-                    <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
+                    <h1>Cadastrar novo caso</h1>
+                    <p>Descreva o caso detalhadamente para encontrar um herói para resolver isso.</p>
 
                     <Link className="back-link" to="/">
                         <FiArrowLeft size={16} color="#E02041" />
-                        Não tenho cadastro
+                        Voltar para home
                     </Link> 
                 </section>
 
                 <form>
-                    <input placeholder="Nome da ONG" />
-                    <input type="email" placeholder="E-mail" />
-                    <input placeholder= "WhatsApp" /> 
-
-                    <div className="input-group">
-                        <input placeholder="Cidade" />
-                        <input placeholder="UF" style= {{ width: 80 }} />  
-                    </div>
+                    <input placeholder="Título do caso" />
+                    <textarea placeholder="Descrição" /> 
+                    <input placeholder="Valor em reais" />
 
                     <button className="button" type="submit" >Cadastrar</button>
                 </form>
