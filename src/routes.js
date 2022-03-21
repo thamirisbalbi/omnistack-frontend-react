4b49fc216d7c76d59b2b7c7c92c8932de631a3de
Thamiris Balbi <thamiris.balbi@estudante.ufjf.br>
2022-03-21 00:48:02 -0300

    Importa profile para routes.js e cria nova rota

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Profile from './pages/Profile';
 
 export default function Routes() { //as rotas viram componentes
     return (
@@ -10,6 +11,8 @@ export default function Routes() { //as rotas viram componentes
             <Switch>
                 <Route path="/" exact component={Login} /> 
                 <Route path="/register" component={Register} />
+
+                <Route path="/profile" component={Profile} /> 
             </Switch>
         </BrowserRouter>
     );
