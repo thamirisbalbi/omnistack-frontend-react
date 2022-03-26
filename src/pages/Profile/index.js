4caadfa19f6b345622cbd3ad55ea710a46f25b22
Thamiris Balbi <thamiris.balbi@estudante.ufjf.br>
2022-03-25 23:04:11 -0300

    Estrutura função para aparecer casos da ong logada

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,13 +1,28 @@
-import React, {useEffect} from 'react'; 
+import React, { useState, useEffect } from 'react'; 
 import { Link } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import './styles.css';
 
 import logoImg from '../../assets/logo.png';
 
 export default function Profile() {
+    const [incidents, setIncidents] = useState([]); //começa com array vazio, pois está buscando informações do backend e precisa começar com um valor válido (vazio).
+
+    const ongId = localStorage.getItem('ongId');
     const ongName = localStorage.getItem('ongName');
+
+    useEffect(() => {
+        api.get('profile', {
+            headers: {
+                Authorization: ongId, //pegar todos os incidentes, passando a rota profile e a organização que está logada no authorization
+            }
+        }).then(response => {
+            setIncidents(response.data); //response.data é o array que retorna de dentro do api
+        }) //pega a resposta e grava os seus dados no estado
+    }, [ongId]); //para o React, é útil colocar ongId como dependência, por ser uma variável que se usa no useEffect, caso o Id da ong mude, é possível recalcular o perfil mostrando os incidentes passados, por exemplo.
 
     return (
         <div className="profile-container">
